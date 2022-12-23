4e70f2590b555b53ed4b39cde6734bd9969c38ab
Thyago Lucas <thyago.devin@gmail.com>
2022-12-23 11:47:32 -0400

    fix: console on token controller

--- src/controllers/authentication_controller.ts
+++ src/controllers/authentication_controller.ts
@@ -16,7 +16,7 @@ export async function user_login(req:Request, res:Response){
 
 	const login_datas = req.body as User_toLogin;
 	const token = await authServ.user_login(login_datas);
-	console.log(token);
+	
 	res.send({token:token}).status(200);
 	
 }
